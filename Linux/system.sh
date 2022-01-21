4b090975608da80cffabddaa3a7e30455fc4ba11
thom1678 <91933325+thom1678@users.noreply.github.com>
2022-01-21 11:53:51 -0500

    Create system.sh

---
+++ Linux/system.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+    [free -h > ~/backups/freemem/free_mem.txt]
+    [df -h > ~/backups/diskuse/disk_usage.txt]
+    [ls -lh > ~/backups/openlist/open_list.txt]
+    [df -k > ~/backups/freedisk/free_disk.txt]
