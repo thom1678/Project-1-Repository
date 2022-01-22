197eefed0eb3537e3c02e4650a830313ae720960
thom1678 <91933325+thom1678@users.noreply.github.com>
2022-01-22 13:49:50 -0500

    Delete Linux directory

--- Linux/sysctl.conf.md
+++
@@ -1,3 +0,0 @@
-This is a screenshot of my sysctl.conf file
-
-![sysctl conf](https://user-images.githubusercontent.com/91933325/150591221-28a9c6e8-336e-4258-90ec-a2d03a75f39c.PNG)
--- Linux/system.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-    [free -h > ~/backups/freemem/free_mem.txt]
-    [df -h > ~/backups/diskuse/disk_usage.txt]
-    [ls -lh > ~/backups/openlist/open_list.txt]
-    [df -k > ~/backups/freedisk/free_disk.txt]
